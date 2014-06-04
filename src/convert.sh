a08c25279d61aeeca52c63839e08439608f8c9fd
Maarten de Waard <mrtndwrd@gmail.com>
2014-06-04 16:44:01 +0200

    removed data that should not have been tracked, added hog extraction, convert now outputs to raw folder

--- src/convert.sh
+++ src/convert.sh
@@ -2,7 +2,7 @@
 
 # require input directory with the jpg files to convert
 if [ $# -lt 2 ]; then
-	echo 'Usage: ./convert.sh input_pdf output_folder'
+	echo 'Usage: ./convert.sh input_pdf book_name'
 	exit 1
 fi
 
@@ -22,15 +22,21 @@ if [ "$f_ext" != 'pdf' ]; then
 	exit 3
 fi
 
+# Write to raw data directory with the book name as root
+directory="../data/$2/raw/"
+
 # Create the output directory, if needed
-if [ ! -d "$2" ]; then
-	mkdir $2
+if [ ! -d "$directory" ]; then
+	echo "Creating directory $directory"
+	mkdir "$directory" || { echo 'ERROR'; exit; }
 fi
 
-# loop over all pages in the pdf
-for (( i=1; i<=$numPages; i++ ))
+# loop over all pages in the pdf. Skip the first 2 pages, since they contain
+# Google information
+for (( i=2; i<$numPages; i++ ))
 do
-	f="$2`printf '%04d' $i`.png"
+	f="$directory`printf '%04d' $i`.png"
+	echo "Exporting file $f"
 
 	# to check if the output file already exists, we'll just stop
 	if [ -f $f ]; then
