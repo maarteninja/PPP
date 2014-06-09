3329f352aa858273d69443a718d1bc5c8ab3ef02
Maarten de Waard <mrtndwrd@gmail.com>
2014-06-09 18:44:21 +0200

    almost working learning svm classifier. Problem is that the number of features is not always exactly the same

--- src/extracthog.py
+++ src/extracthog.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os, glob
 import numpy as np
+import argparse
 
 import Image
 
@@ -11,64 +12,148 @@ from skimage import data, color, exposure
 
 from sklearn import svm
 
-PIXELS_PER_CELL = (60,60)
+class BookLearner:
 
-def calculate_and_show_hog(image):
-	image = color.rgb2gray(image)
+	def __init__(self, input_folder, pixels_per_cell=(62,50)):
+		""" The input folder is the folder containing all the books. The
+		pixels_per_cell are used for the hog features. This tuple is (62,50) by
+		default, which will result in 10x10 hog features per page
+		"""
+		self.input_folder = input_folder
+		self.pixels_per_cell = pixels_per_cell
+		self.classifier = svm.SVC(probability=1)
+		books = os.listdir(self.input_folder)
+		# Take 80 percent as train set:
+		train_end = int(len(books)*.8)
+		# Check if train_end is not the same as len(books)-1: Then we wouldn't
+		# have a test set
+		if train_end == len(books) - 1:
+			train_end = train_end - 1
+		# self.train_set = books[0:train_end]
+		# self.test_set = books[train_end:len(books)]
+		self.train_set = ['naauwKeurigeAanteekeningen']
+		self.test_set = ['journaelOfDaghRegister']
+		self.all_descriptors = []
+		self.all_labels = []
 
-	fd, hog_image = hog(image, orientations=8, pixels_per_cell=PIXELS_PER_CELL,
-						cells_per_block=(1, 1), visualise=True)
 
-	fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+	def train(self):
+		""" Trains the svm. self.train_set is used as the training set """
+		for book in self.train_set:
+			# If the book is annotated, this directory should exist:
+			if(os.path.exists(self.input_folder + os.sep + book + os.sep + 'annotated') and 
+				os.path.isdir(self.input_folder + os.sep + book + os.sep + 'annotated')):
+				# Then read its descriptors and labels
+				print "reading book %s" % (book)
+				descriptors, labels = self.read_book_data(book)
+				self.all_descriptors.extend(descriptors)
+				self.all_labels.extend(labels)
+			else:
+				print 'no data for book %s' % (book)
+		self.all_descriptors = np.array(self.all_descriptors)
+		for descriptor in self.all_descriptors:
+			print len(descriptor)
 
-	ax1.axis('off')
-	ax1.imshow(image, cmap=plt.cm.gray)
-	ax1.set_title('Input image')
+		# print self.all_descriptors
+		# print np.shape(self.all_descriptors)
+		# print np.shape(self.all_labels)
+		# # Fit the classifier:
+		self.classifier.fit(self.all_descriptors, self.all_labels)
 
-	# Rescale histogram for better display
-	hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+	def test(self):
+		""" Tests the trained svm on the test set in self.test_set. train (or
+		some kind of "load" function  in later versions) has to be run first!
+		"""
+		test_descriptors = []
+		test_real_labels = []
+		for book in self.test_set:
+			if(os.path.exists(self.input_folder + os.sep + book + os.sep + 'annotated') and 
+				os.path.isdir(self.input_folder + os.sep + book + os.sep + 'annotated')):
+				# Then read its descriptors and labels
+				print "reading book %s" % (book)
+				descriptors, labels = self.read_book_data(book)
+				test_descriptors.extend(descriptors)
+				test_real_labels.extend(labels)
+			else:
+				print 'no data for book %s' % (book)
+		# print test_descriptors
+		print np.shape(test_descriptors)
+		print np.shape(test_real_labels)
+		test_predicted_labels = self.classifier.predict(test_descriptors)
+		print test_real_labels
+		print test_predicted_labels
+		correct = wrong = 0
+		for i in range(1, len(test_real_labels)):
+			if(test_real_labels[i] == test_predicted_labels[i]):
+				correct += 1
+			else:
+				wrong += 1
+		print "Correct: %d, False: %d" % (correct, false)
 
-	ax2.axis('off')
-	ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
-	ax2.set_title('Histogram of Oriented Gradients')
-	plt.show()
-	return fd
 
-def calculate_hog(image):
-	image = color.rgb2gray(image)
-	return hog(image, orientations=8, pixels_per_cell=PIXELS_PER_CELL,
-						cells_per_block=(1, 1))
+	def calculate_and_show_hog(self, image):
+		image = color.rgb2gray(image)
+
+		fd, hog_image = hog(image, orientations=8, pixels_per_cell=self.pixels_per_cell,
+							cells_per_block=(1, 1), visualise=True)
+
+		fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+
+		ax1.axis('off')
+		ax1.imshow(image, cmap=plt.cm.gray)
+		ax1.set_title('Input image')
+
+		# Rescale histogram for better display
+		hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+
+		ax2.axis('off')
+		ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+		ax2.set_title('Histogram of Oriented Gradients')
+		plt.show()
+		return fd
+
+	def calculate_hog(self, image):
+		image = color.rgb2gray(image)
+		return hog(image, orientations=8, pixels_per_cell=self.pixels_per_cell,
+							cells_per_block=(1, 1))
+
+	def read_book_data(self, book):
+		""" Read the hog features from the image files, and the class from the
+		python files corresponding to the book pages in the directory 'book'. A
+		folder named 'annotated' needs to be present, with 500_<page>.py as
+		annotated data for the image raw/500_<page>.png 
+		""" 
+		annotated_images = glob.glob(self.input_folder + os.sep + book + os.sep + 'annotated' + os.sep + '*.py')
+		# This array will hold the HOG feature descriptors for each class
+		descriptors= []
+		# This array will hold the labels of the images. Each index in this array
+		# corresponds to the same index in descriptors. The two of them together
+		# form the input for a classifier
+		labels = []
+
+		for annotated_image in annotated_images:
+			with open(annotated_image) as f:
+				data = eval(f.read())
+				labels.append(data['type']);
+				print 'Reading %s' % (annotated_image)
+				# Find the image file
+				base = os.path.basename(annotated_image)
+				name = os.path.splitext(base)[0]
+				image_name = self.input_folder + os.sep + book + os.sep + \
+					'raw' + os.sep + name + '.png'
+				image = misc.imread(image_name);
+				descriptors.append(self.calculate_hog(image));
+
+		return descriptors, labels
 
 if __name__ == '__main__':
-	folder = '../data/atlas/raw'
-	# folder = '../data/atlas/raw/'
-	images = glob.glob(folder + os.sep + '*.png')
-	# This array will hold the HOG feature descriptors for each class
-	descriptors= []
-	# This array will hold the labels of the images. Each index in this array
-	# corresponds to the same index in descriptors. The two of them together
-	# form the input for a classifier
-	labels = []
-	for image in images[1:10]:
-		print 'Reading %s' % (image)
-		im = misc.imread(image);
-		fd = calculate_hog(im)
-		descriptors.append(fd);
-		# Let's act as if all images are text, for the sake of testing
-		labels.append('text');
-	for image in images[11:20]:
-		print 'Reading %s' % (image)
-		im = misc.imread(image);
-		fd = calculate_hog(im)
-		descriptors.append(fd);
-		# Let's act as if all images are text, for the sake of testing
-		labels.append('pic');
-	print descriptors
-	print labels
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_folder", metavar='input_folder', type=str,
+					help="""The input folder.""")
+	
+	args = vars(parser.parse_args())
 
-	# Start the classifier:
-	classifier = svm.SVC(probability=1)
-	classifier.fit(descriptors, labels)
-	im = misc.imread(images[21]);
-	fd = calculate_hog(im)
-	print classifier.predict_log_proba(fd)
+	learner = BookLearner(args['input_folder'])
+	learner.train()
+	learner.test()
+
