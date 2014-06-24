b05de4879a9c1f96747ac60c88884b5f6592bc3b
Maarten Inja <maarten.inja@gmail.com>
2014-06-24 17:49:15 +0200

    image extractor "works", well not really

--- src/imageextractor.py
+++ src/imageextractor.py
@@ -6,6 +6,7 @@ import numpy as np
 from pygame.locals import *
 from PIL import Image
 import os
+from copy import deepcopy
 import pygame
 from annotater import Annotater
 
@@ -30,6 +31,8 @@ def get_image_boxes_from_prediction(labels, boxes, output_folder, overlap):
 
 	image_boxes = {}
 
+	#exit()
+
 	for n in range(labels_shape[0]):
 
 		for i in range(labels_shape[1]):
@@ -39,14 +42,72 @@ def get_image_boxes_from_prediction(labels, boxes, output_folder, overlap):
 				if labels[n][i][j] == 0 and \
 						not(box_in_boxes(image_boxes.get(n, []), (boxes[n][i][j]))):
 					# find largest rectangle
-					new_box = find_box(n, i, j, boxes, labels)
+					#new_box = find_box(n, i, j, boxes, labels)
+					new_box = get_bounding_box_max(n, i, j, boxes, labels)
 					if new_box:
 						l = image_boxes.get(n, [])
 						print 'adding ', new_box, ' to ', l
 						l.append(new_box)
 						image_boxes[n] = l
-	print image_boxes
+
+	#print image_boxes
 	return image_boxes
+
+def get_bounding_box_max(n, i, j, boxes, labels):
+	min_i, max_i, min_j, max_j = maximum_box_indices(n, i, j, labels)
+	top_left = boxes[n][min_i][min_j]
+	bottom_right = boxes[n][max_i][max_j]
+	#print top_left[0], top_left[1], bottom_right[2], bottom_right[3]
+	return top_left[0], top_left[1], bottom_right[2], bottom_right[3]
+
+def maximum_box_indices(n, i, j, labels):
+	indices = get_indices_from(i, j, labels[n])
+	#print 'before', indices
+	indices = sorted(indices, key=lambda x: x[0])
+	#print 'after', indices
+	min_i = indices[0][0]
+	max_i = indices[-1][0]
+	indices = sorted(indices, key=lambda x: x[1])
+	min_j = indices[0][1]
+	max_j = indices[-1][1]
+	#print 'mfkdjsalkfdjsalkfjalkf', min_i, max_i, min_j, max_j
+	return min_i, max_i, min_j, max_j
+
+def get_indices_from(i, j, labels):
+	found = []
+	expand_list = [(i, j)]
+
+	while len(expand_list) > 0:
+		# explore the next indice
+		k, l = expand_list.pop()
+		#print 'current', k, l
+		# add it to the found list
+		found.append((k, l))
+		# explore possible nexts
+		nexts = [(k-1, l), (k+1, l), (k, l-1), (k, l+1)]
+		#print 'n1', nexts
+		# throwaway all already found
+		nexts = filter(lambda x: x not in found, nexts)
+		#print 'n2', nexts
+		# throwaway all illegal values
+		nexts = filter(lambda n: n[0] >= 0 and n[1] >= 0 and n[0] < len(labels) and \
+			n[1] < len(labels[0]), nexts)
+		#print 'n3', nexts
+		# throwaway all values for text
+		nexts = filter(lambda n: labels[n[0]][n[1]] == 0, nexts)
+		#print 'n4', nexts
+		# store the next!
+		expand_list.extend(nexts)
+		#print 'el:', expand_list
+		#print 'f:', found
+	return found
+
+def min_max_index_on_row(i, labels):
+	row = deepcopy(labels[i])
+	mi = row.index(0)
+	row.revese()
+	ma = len(row) - row.index(0)
+	return mi, ma
 
 def find_box(n, i_start, j_start, boxes, labels):
 
@@ -142,7 +203,7 @@ if __name__ == '__main__':
 
 	# read all images from input folder
 	pages_data = bookfunctions.get_pages_and_data_from_book(input_folder)
-	#pages_data = pages_data[:10]
+	#pages_data = pages_data[379:381]
 
 	features = bookfunctions.get_features_from_pages_data(pages_data, \
 		number_of_blocks, overlap, svm_path)
