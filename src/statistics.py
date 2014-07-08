2063b10196e8f72c0a503c3a6c1819d4249aa487
Maarten de Waard <mrtndwrd@gmail.com>
2014-07-08 14:24:21 +0200

    merged annotator and dataset sections, put all figures on one page, normalized look of tables, added testset statistics to dataset section

--- src/statistics.py
+++ src/statistics.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 if __name__ == '__main__':
-	input_folder = '../data'
+	input_folder = '../testset'
 	books = os.listdir(input_folder)
 
 	pages_count = []
@@ -19,7 +19,9 @@ if __name__ == '__main__':
 		pages_count.append(len(pages))
 		images_count.append(len(images))
 
+	print 'number of pages', np.sum(pages_count)
 	print 'pages mean', np.mean(pages_count)
 	print 'pages std', np.std(pages_count)
+	print 'number of images', np.sum(images_count)
 	print 'images mean', np.mean(images_count)
 	print 'images std', np.std(images_count)
